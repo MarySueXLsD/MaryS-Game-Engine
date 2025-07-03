04ca3430d46e4f168c2edb03280102bf5a804727
viktar-rpa <viktar.syanau@hiab.com>
2025-07-03 16:52:42 +0200

    Add placeholder modules and bridge.json files for all planned editor features (feature/module-placeholders branch)

---
+++ Content/Modules/DialogueEditor_essential/DialogueEditor.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
+using MarySGameEngine;
+
+namespace MarySGameEngine.Modules.DialogueEditor_essential
+{
+    public class DialogueEditor : IModule
+    {
+        public void Update() { }
+        public void Draw(SpriteBatch spriteBatch) { }
+        public void UpdateWindowWidth(int width) { }
+        public void LoadContent(ContentManager content) { }
+        public void Dispose() { }
+    }
+} 
---
+++ Content/Modules/ImageEditor_essential/ImageEditor.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
+using MarySGameEngine;
+
+namespace MarySGameEngine.Modules.ImageEditor_essential
+{
+    public class ImageEditor : IModule
+    {
+        public void Update() { }
+        public void Draw(SpriteBatch spriteBatch) { }
+        public void UpdateWindowWidth(int width) { }
+        public void LoadContent(ContentManager content) { }
+        public void Dispose() { }
+    }
+} 
